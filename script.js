a6e1a968f202e2048e7eefb14abd603fc58cd4ea
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-05-20 14:24:30 -0400

    created dataset on book divs to match index of library array to properly compare for removing. Need to implement removing next

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ let addBooksToContainer = (book) =>{
         let bookCardH2 = document.createElement("h2");
         let bookCardH3 = document.createElement("h3");
         let bookCardP = document.createElement("p");
+        let removeButton = document.createElement("button")
+
+        removeButton.addEventListener("mouseup",removeBook)
     for(let i = 0; i<myLibrary.length; i++){
         
         
@@ -26,11 +29,14 @@ let addBooksToContainer = (book) =>{
         bookCardH2.textContent = myLibrary[i].book.title;
         bookCardH3.textContent = myLibrary[i].book.author;
         bookCardP.textContent = myLibrary[i].book.pages;
+        removeButton.textContent = "Remove Book?";
         bookContainer.appendChild(bookCard)
         bookCard.appendChild(bookCardH2)
         bookCard.appendChild(bookCardH3)
         bookCard.appendChild(bookCardP)
+        bookCard.appendChild(removeButton)
         bookCard.id = `book${i}`
+        bookCard.dataset.attribute = `${i}`
     }
 }
 
@@ -38,7 +44,17 @@ let addBookToLibrary = (e) =>{
     let book = new Book(titleInput.value,authorInput.value,pagesInput.value);
     myLibrary = [...myLibrary,({book})]
     addBooksToContainer(book)
-    console.log(myLibrary)
+    // console.log(myLibrary)
+}
+
+let removeBook = (e) => {
+    let currentBook = e.target.parentElement.dataset.attribute;
+    console.log(typeof currentBook)
+    for(let i = 0; i<myLibrary.length; i++){
+        if(i == currentBook){
+            console.log(i,currentBook)
+        }else{console.log("triggr")}
+    }
 }
 
 addBookButton.addEventListener("mouseup",addBookToLibrary)
