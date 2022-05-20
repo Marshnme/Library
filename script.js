aca7017aa28cac6a39b223e0ee489abe721dde0d
Joshua Holtsclaw <jildsmiles@yahoo.com>
2022-05-20 13:22:54 -0400

    each book now has a unique id that I can use for deleting

--- script.js
+++ script.js
@@ -14,13 +14,15 @@ function Book(title,author,pages){
 
 let bookContainer = document.querySelector(".books");
 
-let addBookToContainer = () =>{
-    let bookCard = document.createElement("div");
-    let bookCardH2 = document.createElement("h2");
-    let bookCardH3 = document.createElement("h3");
-    let bookCardP = document.createElement("p");
+let addBooksToContainer = (book) =>{
+        let bookCard = document.createElement("div");
+        let bookCardH2 = document.createElement("h2");
+        let bookCardH3 = document.createElement("h3");
+        let bookCardP = document.createElement("p");
+    for(let i = 0; i<myLibrary.length; i++){
+        
+        
 
-    for(let i =0; i<myLibrary.length; i++){
         bookCardH2.textContent = myLibrary[i].book.title;
         bookCardH3.textContent = myLibrary[i].book.author;
         bookCardP.textContent = myLibrary[i].book.pages;
@@ -28,13 +30,15 @@ let addBookToContainer = () =>{
         bookCard.appendChild(bookCardH2)
         bookCard.appendChild(bookCardH3)
         bookCard.appendChild(bookCardP)
+        bookCard.id = `book${i}`
     }
 }
 
 let addBookToLibrary = (e) =>{
     let book = new Book(titleInput.value,authorInput.value,pagesInput.value);
-    myLibrary.push({book})
-    addBookToContainer(book)
+    myLibrary = [...myLibrary,({book})]
+    addBooksToContainer(book)
+    console.log(myLibrary)
 }
 
 addBookButton.addEventListener("mouseup",addBookToLibrary)
